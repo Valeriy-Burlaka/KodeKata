899d5310f0f5f7e376ffa447cd9abaaaf9882531
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2023-05-05 13:35:42 +0200

    [LRU Cache] better var names

--- typescript/src/lru-cache/naive-lru-cache.ts
+++ typescript/src/lru-cache/naive-lru-cache.ts
@@ -1,54 +1,54 @@
 type Key = number;
 type Value = string;
+type Items = Map<Key, Value>;
+
 export const NoValue = -1;
 
 export class LRUCache {
   public capacity: number;
   public storage: {
-    elements: Map<Key, Value>;
-    elementsOrder: Array<Key>;
+    items: Items;
+    itemsOrder: Array<Key>;
   };
   
   constructor (capacity: number) {
     this.capacity = capacity;
     this.storage = {
-      elements: new Map(),
-      elementsOrder: [],
+      items: new Map(),
+      itemsOrder: [],
     };
   }
   
   public get(key: Key): Value | typeof NoValue {
-    return this.storage.elements.get(key) || NoValue;
+    return this.storage.items.get(key) || NoValue;
   }
   
   private _add(key: Key, value: Value) {
-    this.storage.elements.set(key, value);
-    this.storage.elementsOrder.push(key);
+    this.storage.items.set(key, value);
+    this.storage.itemsOrder.push(key);
   }
   
   public put(key: Key, value: Value) {
-    // The element doesn't exist in our cache
+    // The item doesn't exist in our cache
     if (this.get(key) < 0) {
-      // We have capacity to add a new element - add it!
-      if (this.storage.elementsOrder.length < this.capacity) {
+      // We have capacity to add a new item - add it!
+      if (this.storage.itemsOrder.length < this.capacity) {
         this._add(key, value);
-      // We don't have capacity - remove the oldest elem from the cache and add a new element
+      // We don't have capacity - remove the oldest elem from the cache and add a new item
       } else {
-        const lastElementKey = this.storage.elementsOrder[0];
-        this.storage.elementsOrder.shift();
-        this.storage.elements.delete(lastElementKey);
+        const lastItemKey = this.storage.itemsOrder[0];
+        this.storage.itemsOrder.shift();
+        this.storage.items.delete(lastItemKey);
         this._add(key, value);
       }
-    // The element exists in our cache, so we need to update the value and its order (it becomes last)
+    // The item exists in our cache, so we need to update the value and its order (it becomes last)
     } else {
-      const thisKeyPosition = this.storage.elementsOrder.indexOf(key);
-      // console.log({ thisKeyPosition, currentOrder: this.storage.elementsOrder})
-      const newElementsOrder = [
-        ...this.storage.elementsOrder.slice(0, thisKeyPosition),
-        ...this.storage.elementsOrder.slice(thisKeyPosition + 1),
+      const thisKeyPosition = this.storage.itemsOrder.indexOf(key);
+      const newItemsOrder = [
+        ...this.storage.itemsOrder.slice(0, thisKeyPosition),
+        ...this.storage.itemsOrder.slice(thisKeyPosition + 1),
       ];
-      // console.log({ newElementsOrder })
-      this.storage.elementsOrder = newElementsOrder;
+      this.storage.itemsOrder = newItemsOrder;
       this._add(key, value);
     }
 
