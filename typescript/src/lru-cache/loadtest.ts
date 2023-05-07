343a49fedd87b71eca1a889743b014f79ff78392
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2023-05-07 14:16:49 +0200

    [LRU Cache] update loadtest to run against different cache sizes and collect test results

--- typescript/src/lru-cache/loadtest.ts
+++ typescript/src/lru-cache/loadtest.ts
@@ -4,8 +4,7 @@
 import Benchmark from 'benchmark';
 
 import { LRUCache } from './naive-lru-cache';
-
-const CACHE_SIZE = 1000;
+// import { LRUCache } from './lru-cache';
 
 function randomPick(arr: Array<any>) {
   const randomIndex = Math.floor(Math.random() * arr.length);
@@ -21,7 +20,7 @@ function setupCache(size: number): { cache: LRUCache, testKeys: number[] } {
   const cache = new LRUCache(size);
   const testKeys: number[] = [];
   
-  new Array(CACHE_SIZE).fill(0).forEach((_, index) => {
+  new Array(size).fill(0).forEach((_, index) => {
     cache.put(index, getItemValue(index));
     testKeys.push(index);
   });
@@ -30,49 +29,69 @@ function setupCache(size: number): { cache: LRUCache, testKeys: number[] } {
 }
 
 function testUpdateExistingItemsInCache(cache: LRUCache, testKeys: number[]) {
-  // console.log('Running the test')
   new Array(10000).fill(0).forEach(() => {
     const randomKey = randomPick(testKeys);
     cache.put(randomKey, Number(randomKey).toString(16));
   });
 }
 
-function testPutNewItemToTheFullCache(cache: LRUCache) {
+function testPutNewItemToTheFullCache(cache: LRUCache, testKeys: number[]) {
   new Array(10000).fill(0).forEach((_, index) => {
-    const newKey = CACHE_SIZE + index + 1;
+    const newKey = testKeys.length + index + 1;
     cache.put(newKey, Number(newKey).toString(16));
   });
 }
 
-const suite = new Benchmark.Suite();
-suite.add(
-  'updating existing item in the cache',
-  {
-    fn: function() { testUpdateExistingItemsInCache(this.cache, this.testKeys) },
-    onStart: function() {
-      // console.log('Running "before"')
-      const { cache, testKeys } = setupCache(CACHE_SIZE);
-      
-      this.cache = cache;
-      this.testKeys = testKeys;
-    },
+async function runBenchmark(cacheSize, testResults) {
+  return new Promise((resolve) => {
+    const suite = new Benchmark.Suite();
+    suite.add(
+      'updating existing item in the cache',
+      {
+        fn: function() { testUpdateExistingItemsInCache(this.cache, this.testKeys) },
+        onStart: function() {
+          const { cache, testKeys } = setupCache(cacheSize);
+
+          this.cache = cache;
+          this.testKeys = testKeys;
+        },
+      })
+      .add(
+        'put a new item in the cache (evict the oldest item)',
+        {
+          fn: function() { testPutNewItemToTheFullCache(this.cache, this.testKeys) },
+          onStart: function() {
+            const { cache, testKeys } = setupCache(cacheSize);
+          
+            this.cache = cache;
+            this.testKeys = testKeys;
+          },
+        }
+      )
+      .on('complete', function() {
+        this.forEach(function(test) {
+          if (testResults[test.name]) {
+            testResults[test.name][cacheSize] = test.stats.mean * 1000;
+          } else {
+            testResults[test.name] = {[cacheSize]: test.stats.mean * 1000 }
+          }
+        });
+
+        resolve();
+      })
+      .run({ async: true });
   })
-  .add(
-    'put a new item in the cache (evict the oldest item)',
-    {
-      fn: function() { testPutNewItemToTheFullCache(this.cache) },
-      onStart: function() {
-        // console.log('Running "before"')
-        const { cache } = setupCache(CACHE_SIZE);
-        
-        this.cache = cache;
-      },
-    }
-  )
-  .on('complete', function() {
-    console.log('Finished running tests:');
-    this.forEach(function(test) {
-      console.log(`TEST: "${test.name}": Average time: ${test.stats.mean * 1000} milliseconds`);
-    });
-  })
-  .run({ async: true });
+}
+
+(async function () {
+  const testResults = {};
+
+  const cacheSizes = Array.from({ length: 15 }, (_, i) => (i + 1) * 1000);
+  for (const CACHE_SIZE of cacheSizes) {
+    console.log('Testing cache size: ', CACHE_SIZE);
+     await runBenchmark(CACHE_SIZE, testResults);
+
+  }
+
+  console.log(testResults);
+})();
